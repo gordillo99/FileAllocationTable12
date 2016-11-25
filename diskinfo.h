5ba488fac3d34e0d8e10758bbcb0a596b7c54a16
gordillo99 <metalj99@gmail.com>
2016-11-24 17:43:09 -0800

    cleaned up project

--- diskinfo.h
+++
@@ -1,6 +0,0 @@
-void getOSName(char * p, char * osName);
-int getTotalNumberOfSectors(char * p);
-int getSectorValue(char * p, int i);
-int getFreeSize(int totalNumberOfSectors, char * p);
-void getLabel(char * p, char * label);
-int countDirectoriesInRoot(char * p);
